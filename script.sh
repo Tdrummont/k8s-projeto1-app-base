319325c0969b7f1b9c3f95b7d81084667b550618
tdrummontt <tdrummontt@gmail.com>
2022-12-19 13:55:53 -0300

    first commit

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/
+
+echo "Criando as imagens........"
+
+docker build -t tdrummontt/projeto-backend:1.0 backend/.
+docker build  -t tdrummontt/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagens....."
+
+docker push tdrummontt/projeto-backend:1.0
+docker push tdrummontt/projeto-database:1.0
+
+echo "Criando serviços no cluster kubernetes..."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments......"
+
+kubectl apply -f ./deployments.yml
+
